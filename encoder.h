394fabb4364b82a1f48bd2c900d32442779ffb66
Rohan Bhardwaj <rohanbhardwaj720@gmail.com>
2024-06-14 14:01:51 +0530

    encoder function is updated so that now it can encode the splitted_string

--- encoder.h
+++ encoder.h
@@ -5,7 +5,7 @@
 #include <vector>
 #include <map>
 
-std::map<int, std::pair<int, int>> encoder(std::vector<int> &compressed, int n_merges) {
+std::map<int, std::pair<int, int>> encoder(std::vector< std:: vector<int> > &compressed, int n_merges) {
     std::map<int, std::pair<int, int>> merges;
     int val = 256;
 
@@ -16,12 +16,14 @@ std::map<int, std::pair<int, int>> encoder(std::vector<int> &compressed, int n_m
         std::pair<int, int> max_pair;
 
         // Count pairs and find the most frequent pair
-        for (int i = 0; i + 1 < compressed.size(); i++) {
-            count_pairs[compressed[i]][compressed[i + 1]]++;
+        for(int j = 0; j<compressed.size(); j++){
+            for (int i = 0; i + 1 < compressed[j].size(); i++) {
+                count_pairs[compressed[j][i]][compressed[j][i + 1]]++;
 
-            if (max_count < count_pairs[compressed[i]][compressed[i + 1]]) {
-                max_pair = std::make_pair(compressed[i], compressed[i + 1]);
-                max_count = count_pairs[compressed[i]][compressed[i + 1]];
+                if (max_count < count_pairs[compressed[j][i]][compressed[j][i + 1]]) {
+                    max_pair = std::make_pair(compressed[j][i], compressed[j][i + 1]);
+                    max_count = count_pairs[compressed[j][i]][compressed[j][i + 1]];
+                }
             }
         }
 
@@ -29,21 +31,22 @@ std::map<int, std::pair<int, int>> encoder(std::vector<int> &compressed, int n_m
         merges[val] = std::make_pair(max_pair.first, max_pair.second);
 
         // Update the compressed vector with merged pairs
-        std::vector<int> temp;
+        std::vector< std:: vector<int> > temp;
 
-        for (int i = 0; i < compressed.size();) {
-            if (compressed[i] == max_pair.first && compressed[i + 1] == max_pair.second) {
-                temp.push_back(val);
-                i += 2;
-            } else {
-                temp.push_back(compressed[i]);
-                i++;
+        for(int j = 0; j<compressed.size(); j++){
+            for (int i = 0; i < compressed[j].size();) {
+                if (compressed[j][i] == max_pair.first && compressed[j][i + 1] == max_pair.second) {
+                    temp[j].push_back(val);
+                    i += 2;
+                } else {
+                    temp[j].push_back(compressed[j][i]);
+                    i++;
+                }
             }
         }
 
         val++;
         compressed = temp;
-        
     }
 
     return merges;
--- test.cpp
+++ test.cpp
@@ -23,41 +23,63 @@ int main() {
     // input string
     std::getline(inputFile, input);
     
-    std::vector<int> utf8Bytes = convertToUtf8Bytes(input);
+    // string is divided, so the unwanted merges will not take place
+    std::vector<std::string> splitted_string = patternParser(input);
 
-    outputFile << "UTF-8 Encoded Bytes: ";
-    for (size_t i = 0; i < utf8Bytes.size(); ++i) {
-        outputFile << utf8Bytes[i] << " ";
+    // UTF-8 conversion
+    std::vector<std:: vector<int> > utf8Bytes;
+    std::vector<int> temp;
+
+    for(int i=0; i< splitted_string.size();i++){
+        temp = convertToUtf8Bytes(splitted_string[i]);
+        utf8Bytes.push_back(temp);
     }
-    outputFile << std::endl;
-    outputFile << convertBytesToString(utf8Bytes);
-    outputFile << std::endl;
+
+    outputFile << "UTF-8 Expression of Splitted String: " << std:: endl;
+    for (int i = 0; i < utf8Bytes.size(); ++i) {
+
+        outputFile << "'" << splitted_string[i] << "' : ";
+        for(int j=0; j< utf8Bytes[i].size(); ++j){
+            outputFile << utf8Bytes[i][j] <<" ";
+        }
+        outputFile << std:: endl;
+    }
+
+    int new_vocab_size;
+    // input new vocab size >= 256
+    inputFile >> new_vocab_size;
+    int n_merges = new_vocab_size - 256;
+
+
 
     // To store the compressed form of expression after merges
-    std::vector<int> compressed = utf8Bytes;
-    
-    int new_vocab_size;
-    // input new vocab size>=256
-    inputFile >> new_vocab_size;
+    std::vector<std::string> compressed_splitted_string = splitted_string;
 
-    int n_merges = new_vocab_size - 256;
-    
+    std::map<int, std::pair<int, int>> new_vocab = encoder(compressed_splitted_string, n_merges);
 
-    // std::map<int, std::pair<int, int>> new_vocab = encoder(compressed, n_merges);
-
-    // for (auto it = new_vocab.begin(); it != new_vocab.end(); ++it) {
-    //     int a = it->first;
-    //     std::pair<int, int> b = it->second;
-    //     outputFile << a << ": " << b.first << " " << b.second << std::endl;
-    // }
-    
-    // if (utf8Bytes == decoder(compressed, new_vocab)) {
-    //     outputFile << "passed";
-    // }
-    std::vector<std::string> tokens = patternParser(input);
-    for (const auto& token : tokens) {
-        std::cout << "Token: '" << token <<"'"<< std::endl;
+    for (auto it = new_vocab.begin(); it != new_vocab.end(); ++it) {
+        int a = it->first;
+        std::pair<int, int> b = it->second;
+        outputFile << a << ": " << b.first << " " << b.second << std::endl;
     }
+    
+    if (utf8Bytes == decoder(compressed_splitted_string, new_vocab)) {
+        outputFile << "passed" << std::endl;
+    }
+    
+    int final_len = 0;
+    for(int i = 0; i < compressed_splitted_string.size(); i++){
+        final_len += compressed_splitted_string[i].size();
+    }
+
+    int initial_len;
+    for(int i = 0; i < utf8Bytes.size(); i++){
+        initial_len += utf8Bytes[i].size();
+    }    
+
+    float compression_ratio = initial_len/(float)final_len;
+    outputFile << "Compression Ratio: " << compression_ratio << std::endl;
+
     inputFile.close();
     outputFile.close();
 
